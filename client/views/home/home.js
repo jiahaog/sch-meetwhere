83e9b4c13b5dc0dea937883750e703dca9b5ae36
Qin Cheng <pqc92@hotmail.com>
2016-01-11 22:21:44 +0800

    added maps api

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -3,7 +3,38 @@ Template.home.helpers({
 });
 
 Template.home.onRendered(function () {
+  var pyrmont = new google.maps.LatLng(-33.8665, 151.1956);
 
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15,
+    scrollwheel: false
+  });
+
+  // Specify location, radius and place types for your Places API search.
+  var request = {
+    location: pyrmont,
+    radius: '500',
+    types: ['store']
+  };
+
+  // Create the PlaceService and send the request.
+  // Handle the callback with an anonymous function.
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        // If the request succeeds, draw the place location on
+        // the map as a marker, and register an event to handle a
+        // click on the marker.
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+      }
+    }
+  });
 });
 
 Template.home.events({
