1c83ee65d21e6f4220c79f8354ced6ef4f947938
tanhaoqin <tanhq646@gmail.com>
2016-01-12 10:48:35 +0800

    Added Google server side calls

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,4 +1,10 @@
+Locs = new Mongo.Collection("locs");
+
 Template.home.helpers({
+	locs: function() {
+		return Locs.find({});
+	},
+
 
 });
 
@@ -23,6 +29,15 @@ Template.home.onRendered(function () {
   var infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   
+	function bindInfoWindow(marker, map, infowindow, place) {
+	    marker.addListener('click', function() {
+	        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+	        'Place ID: ' + place.place_id + '<br>' +
+	        place.formatted_address + '</div>');
+	        infowindow.open(map, this);
+	    });
+	}
+
   service.nearbySearch(request, function(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
@@ -41,15 +56,29 @@ Template.home.onRendered(function () {
   });
 });
 
-function bindInfoWindow(marker, map, infowindow, place) {
-    marker.addListener('click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        'Place ID: ' + place.place_id + '<br>' +
-        place.formatted_address + '</div>');
-        infowindow.open(map, this);
-    });
-}
+
 
 Template.home.events({
-
+	"submit .new-loc": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      // Locs.insert({
+      //   text: text,
+      //   createdAt: new Date() // current time
+      // });
+	  Meteor.call("getPlace", text, function(error, results){
+	  	var resjson = JSON.parse(results.content);
+	  	var best = resjson.predictions[0];
+	  	Meteor.call("getPlaceDetails", best.place_id, function(err, details){
+	  		console.log(details);
+	  	});
+	  });
+      // Clear form
+      event.target.text.value = "";
+    }
 });
