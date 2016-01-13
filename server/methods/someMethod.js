42ce8c197ecd872bc2c87863d1c216625e8e29cd
tanhaoqin <tanhq646@gmail.com>
2016-01-13 12:43:22 +0800

    Added nearest cluster function

--- server/methods/someMethod.js
+++ server/methods/someMethod.js
@@ -1,5 +1,7 @@
 API_KEY = 'AIzaSyB5T8Fuz3xppG_roO3hL2V104LSi2sYuT8';
 
+clusters = [[1.30616758,103.7993302,],[1.3757285,103.94929786],[1.46722937,103.75735525],[1.31562227,103.87131227],[1.38902096,103.74627578],[1.43068309,103.83036154],[1.25968872,103.82267311],[1.34024457,103.70590711],[1.31259752,103.83817286],[1.3631016,103.84265797],[1.34382476,103.74514933],[1.33782573,103.94872837],[1.369021,103.98493826],[1.33348877,103.78556447],[1.30797351,103.90999824],[1.44161735,103.71061211],[1.28964282,103.85137356],[1.3733761,103.87942697],[1.43888563,103.78901285]]
+
 Meteor.methods({
     /**
      * Performs search for text
@@ -45,7 +47,7 @@ Meteor.methods({
     },
 
     getCenterAndFeaturesFromLatLong: function (locations) {
-        const center = average(locations);
+        const center = closestCluster(locations);
         return {
             center: center,
             features: Meteor.call('apiGetNearby', center).results
@@ -77,4 +79,23 @@ function average(locations) {
 
     const result = [lat, lng];
     return result;
+}
+
+function closestCluster(locations) {
+    var best = 99999
+    var best_cluster = -1
+    const number = locations.length;
+
+    for(var i = 0; i < clusters.length; i++){
+        var dist = 0
+        locations.forEach(location => {
+            dist += Math.pow(location[0]-clusters[i][0],2)+Math.pow(location[1]-clusters[i][1],2)
+        });
+        dist/=number
+        if (dist < best){
+            best = dist;
+            best_cluster = i;
+        }
+    };
+    return clusters[best_cluster]
 }
